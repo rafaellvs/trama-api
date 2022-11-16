a49fa60961dcb3d33ea7d4e59d10d41824cf1cab
Rafael Vieira <rafaellvs322@gmail.com>
2022-11-16 09:32:59 -0300

    Initial

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+import subjectRouter from './routes/subject.js'
+import categoryRouter from './routes/category.js'
+import todoRouter from './routes/todo.js'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Jansen')
+})
+
+app.use('/subject', subjectRouter)
+app.use('/category', categoryRouter)
+app.use('/todo', todoRouter)
+
+app.listen(3000, () => {
+  console.log('API listening at port 3000...')
+})
