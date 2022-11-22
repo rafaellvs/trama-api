de8269c3f096d3457aa1ab91af921afabe8db0b7
Rafael Vieira <rafaellvs322@gmail.com>
2022-11-22 13:04:58 -0300

    FIX: add return statements at controller

--- src/routes/category.js
+++ src/routes/category.js
@@ -13,49 +13,49 @@ const router = Router()
 router.get('/:id', (req, res) => {
   const { id } = req.params
 
-  if (typeof id !== 'number') res.status(400).send('Id must be a number.')
+  if (typeof id !== 'number') return res.status(400).send('Id must be a number.')
 
   const category = getById(id)
 
-  res.send(category)
+  return res.send(category)
 })
 
 router.get('/', (req, res) => {
   const categories = getAll()
 
-  res.send(categories)
+  return res.send(categories)
 })
 
 router.post('/create', (req, res) => {
   const { name, description } = req.body
 
-  if (!name) res.status(400).send('Name is required.')
+  if (!name) return res.status(400).send('Name is required.')
 
   const category = create(name, description)
 
-  res.send(category)
+  return res.send(category)
 })
 
 router.patch('/update/:id', (req, res) => {
   const { id } = req.params
   const { name, description } = req.body
 
-  if (typeof id !== 'number') res.status(400).send('Id must be a number.')
-  if (!name) res.status(400).send('Name is required.')
+  if (typeof id !== 'number') return res.status(400).send('Id must be a number.')
+  if (!name) return res.status(400).send('Name is required.')
 
   const category = update(id, name, description)
 
-  res.send(category)
+  return res.send(category)
 })
 
 router.delete('/remove/:id', (req, res) => {
   const { id } = req.params
 
-  if (typeof id !== 'number') res.status(400).send('Id must be a number.')
+  if (typeof id !== 'number') return res.status(400).send('Id must be a number.')
 
   const category = remove(id)
 
-  res.send(category)
+  return res.send(category)
 })
 
 export default router
