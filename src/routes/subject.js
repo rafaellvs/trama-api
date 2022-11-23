81f9dea4c51f61613ddf87abd2ab94e7989b185a
Rafael Vieira <rafaellvs322@gmail.com>
2022-11-23 16:03:06 -0300

    FEA: add subject controller and routes

--- src/routes/subject.js
+++ src/routes/subject.js
@@ -1,9 +1,55 @@
 import { Router } from 'express'
 
+import {
+  getById,
+  getAll,
+  create,
+  update,
+  remove,
+} from '../controller/subject.js'
+
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.send('Subject GET')
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+
+  const subject = await getById(id)
+
+  return res.send(subject)
+})
+
+router.get('/', async (req, res) => {
+  const subjects = await getAll()
+
+  return res.send(subjects)
+})
+
+router.post('/create', async (req, res) => {
+  const { name, description, categoryId } = req.body
+
+  if (!name) return res.status(400).send('name is required.')
+  if (!categoryId) return res.status(400).send('categoryId is required.')
+
+  const subject = await create(name, description, categoryId)
+
+  return res.send(subject)
+})
+
+router.patch('/update/:id', async (req, res) => {
+  const { id } = req.params
+  const { name, description, categoryId } = req.body
+
+  const response = await update(id, name, description, categoryId)
+
+  return res.send(response)
+})
+
+router.delete('/remove/:id', async (req, res) => {
+  const { id } = req.params
+
+  const response = await remove(id)
+
+  return res.send(response)
 })
 
 export default router
