7da6d8139d55d05c77bc26841c0befe2d45b296d
Rafael Vieira <rafaellvs322@gmail.com>
2022-12-25 14:55:33 -0300

    FEA: add move controller logic to services folder

---
+++ src/services/subject.js
@@ -0,0 +1,92 @@
+import { pool } from './_db-connection.js'
+import { formatSetQueryParams } from '../utils/index.js'
+
+const getById = async (id) => {
+  const querySubject = `
+    SELECT * FROM subject 
+    WHERE id=${id};
+  `
+  const queryRefs = `
+    SELECT * FROM refs
+    WHERE subject_id=${id};
+  `
+  const responseSubject = await pool.query(querySubject)
+  if (!responseSubject.rows.length) return undefined
+  const responseRefs = await pool.query(queryRefs)
+
+  const response = {
+    ...responseSubject.rows[0],
+    refs: responseRefs.rows,
+  }
+
+  return response
+}
+
+const getAll = async () => {
+  const querySubject = `
+    SELECT * FROM subject;
+  `
+  const queryRefs = `
+    SELECT * FROM refs;
+  `
+  const responseSubject = await pool.query(querySubject)
+  if (!responseSubject.rows.length) return []
+  const responseRefs = await pool.query(queryRefs)
+
+  const response = responseSubject.rows.map(subject => {
+    const refs = responseRefs.rows.filter(ref => ref.subject_id === subject.id)
+
+    return {
+      ...subject,
+      refs,
+    }
+  })
+
+  return response
+}
+
+const create = async (name, description, category_id) => {
+  const query = `
+    INSERT INTO subject(name, description, category_id) 
+    VALUES('${name}', '${description}', ${category_id})
+    RETURNING *;
+  `
+  const response = await pool.query(query)
+
+  return response.rows[0]
+}
+
+const update = async (id, name, description, category_id) => {
+  const query = `
+    UPDATE subject 
+    SET ${formatSetQueryParams([
+      { name: 'name', content: name },
+      { name: 'description', content: description },
+      { name: 'category_id', content: category_id },
+    ])}
+    WHERE id=${id}
+    RETURNING *;
+  `
+  const response = await pool.query(query)
+
+  return response.rows[0]
+}
+
+const remove = async (id) => {
+  const query = `
+    DELETE FROM subject 
+    WHERE id=${id}
+    RETURNING *;
+  `
+  const response = await pool.query(query)
+
+  return response.rows[0]
+}
+
+export {
+  getById,
+  getAll,
+  create,
+  update,
+  remove,
+}
