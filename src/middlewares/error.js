a5a8d6e9032b9d2d8045f954f170b0765838f6dd
Rafael Vieira <rafaellvs322@gmail.com>
2023-01-17 16:14:28 -0300

    OTH: errorMiddleware return object

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,9 +1,8 @@
-
 const errorMiddleware = (error, req, res, next) => {
   if (error.name === 'TypeError') return res.status(400).send({ error: error.message })
   if (error.cause === '404') return res.status(404).send({ error: error.message })
 
-  return res.status(500).send(error.message)
+  return res.status(500).send({ error: error.message })
 }
 
 export { errorMiddleware }
