32ddce30ef785c0db2c8120d8355394c1bbe6534
Rafael Vieira <rafaellvs322@gmail.com>
2023-01-19 19:27:10 -0300

    FEA: Add verifyToken controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,4 @@
-import { authenticateUser } from '../services/auth.js'
+import { authenticateUser, verifyJwt } from '../services/auth.js'
 
 const login = async (req, res, next) => {
   const { username, password } = req.body
@@ -18,4 +18,14 @@ const login = async (req, res, next) => {
   }
 }
 
-export { login }
+const verifyToken = async (req, res, next) => {
+  try {
+    const { token } = req.body
+    await verifyJwt({ token })
+    res.status(200).send()
+  } catch (err) {
+    next(new Error(`Unauthorized: ${err.message}`, { cause: '401' }))
+  }
+}
+
+export { login, verifyToken }
