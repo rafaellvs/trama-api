e25c2f24dea61bfe335763ced39fee5962321ad5
Rafael Vieira <rafaellvs322@gmail.com>
2023-02-01 18:52:04 -0300

    FEA: add custom error classes

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,9 +1,56 @@
-const errorMiddleware = (error, req, res, next) => {
-  if (error.name === 'TypeError') return res.status(400).send({ error: error.message })
-  if (error.cause === '401') return res.status(401).send({ error: error.message })
-  if (error.cause === '404') return res.status(404).send({ error: error.message })
+class CustomError extends Error {
+  constructor (message) {
+    super(message)
 
-  return res.status(500).send({ error: error.message })
+    this.name = this.constructor.name
+    this.message = message
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, CustomError)
+    }
+  }
 }
 
-export { errorMiddleware }
+class ValidationError extends CustomError {
+  constructor (message) {
+    super(message)
+    this.status = 400
+  }
+}
+
+class UnauthorizedError extends CustomError {
+  constructor (message) {
+    super(message)
+    this.status = 401
+  }
+}
+
+class NotFoundError extends CustomError {
+  constructor (message) {
+    super(message)
+    this.status = 404
+  }
+}
+
+const errorMiddleware = (error, req, res, next) => {
+  console.error(error)
+
+  const responseConfig = error instanceof CustomError
+    ? { status: error.status, message: error.message }
+    : { status: 500, message: 'Erro desconhecido. Por favor, tente novamente mais tarde.' }
+
+  return res
+    .status(responseConfig.status)
+    .send({ error: responseConfig.message })
+}
+
+process.on('uncaughtException', error => {
+  console.error('Uncaught exception: ', error)
+})
+
+export {
+  ValidationError,
+  UnauthorizedError,
+  NotFoundError,
+  errorMiddleware,
+}
